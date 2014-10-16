b3876f9aaa8eed9ded2be28ef1d95704350118ec
jasonalmaturner <jasonalmaturner@gmail.com>
2014-10-15 18:45:00 -0600

    working on step 3

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express'),
+	app = express(),
+	firebase = require('firebase'),
+	twilio = require('twilio'),
+	bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+var server = app.listen(9001, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('TextSupport app listening at http://%s:%s', host, port);
+})
+
+var myFirebaseRef = new Firebase("https://text-support-9001.firebaseio.com/");
---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+var app = angular.module('TextSupport', ['ngRoute', 'firebase']);
+
+app.config(['$routeProvider', function($routeProvider){
+
+	$routeProvider
+		.when('/', {
+			templateUrl: '/templates/home.html',
+			controller: 'mainCtrl'
+		})
+		.when('/support', {
+			templateUrl: 'templates/support.html',
+			controller: 'supportCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+
+}])
